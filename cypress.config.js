04d52db5936ceb6635c1d72b6d3bc83b74169e12
Suelen Francisco Marques <59808425+Suelen-Francisco-Marqes@users.noreply.github.com>
2023-02-21 22:14:37 -0300

    bug do teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  viewportHeight: 880,
-  viewportWidth: 1280,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    baseUrl: 'http://localhost/techtext-tcs-senac/Frontend/index.php',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
     },
+    experimentalRunAllSpecs: true,
   },
+  fixturesFolder: false,
+  video: false,
 })
